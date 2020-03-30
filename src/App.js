dc741a9fcd6fe97bb5adef43f1d5ae7402dadabd
Francesca <francescabrondi@gmail.com>
2020-03-30 10:48:52 +0200

    Quest_010

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import Input from "./components/Input";
+import SignUp from "./components/SignUp";
 import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/register" component={Input} />
+        <Route exact path="/register" component={SignUp} />
         <Route exact path="/login" component={Login} />
       </Switch>
     </div>
